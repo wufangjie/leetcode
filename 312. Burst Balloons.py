7b1694a7bddcf1c7539f6b33e2756a23b24af011
wfj <wufangjie1223@126.com>
2017-07-14 07:26:16 +0800

    construct one by one dp

--- 312. Burst Balloons.py
+++ 312. Burst Balloons.py
@@ -1,10 +1,57 @@
+from utils import memo
+
+
 class Solution(object):
     def maxCoins(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        # n = len(nums)
+        nums = [elem for elem in nums if elem > 0]
+        if not nums:
+            return 0
+        n = len(nums)
+        nums.append(1)
+        dp = []
 
-        # def rec(lo, hi):
-        #     for i in range(lo, hi):
+        for i in range(n):
+            dp.append([0] * (i + 1))
+            dp[i][i] = nums[i - 1] * nums[i] * nums[i + 1]
+            for j in range(i - 1, -1, -1):
+                left_right = nums[j - 1] * nums[i + 1]
+                theMax = max(dp[i][j + 1] + left_right * nums[j],
+                             dp[i - 1][j] + left_right * nums[i])
+                for k in range(j + 1, i):
+                    theMax = max(theMax, left_right * nums[k] \
+                                 + dp[k - 1][j] + dp[i][k + 1])
+                dp[i][j] = theMax
+        return dp[-1][0]
+
+
+        # # TLE 33/70
+        # nums = [elem for elem in nums if elem != 0]
+        # cache = {}
+
+        # def dfs(*nums):
+        #     n = len(nums)
+        #     if n < 2:
+        #         return sum(nums)
+        #     elif n == 2:
+        #         return nums[0] * nums[1] + max(nums)
+        #     if nums in cache:
+        #         return cache[nums]
+        #     ret = max(nums[0] * nums[1] + dfs(*nums[1:]),
+        #               nums[-1] * nums[-2] + dfs(*nums[:-1]))
+        #     for i in range(1, n - 1):
+        #         ret = max(ret, nums[i] * nums[i - 1] * nums[i + 1]
+        #                   + dfs(*(nums[:i] + nums[i+1:])))
+        #     cache[nums] = ret
+        #     return ret
+
+        # return dfs(*nums)
+
+
+
+#assert Solution().maxCoins([3, 1, 5, 8]) == 167
+print(Solution().maxCoins([8,2,6,8,9,8,1,4,1,5,3,0,7,7,0,4,2,2]))
+print(Solution().maxCoins([8,2,6,8,9,8,1,4,1,5,3,0,7,7,0,4,2,2,5,5]))
--- 546. Remove Boxes.py
+++ 546. Remove Boxes.py
@@ -1,6 +1,4 @@
-from collections import defaultdict, Counter
-from copy import deepcopy
-from utils import memo
+from collections import defaultdict
 
 
 class Solution(object):
@@ -9,6 +7,58 @@ class Solution(object):
         :type boxes: List[int]
         :rtype: int
         """
+        unq, cnt = [], []
+        for b in boxes:
+            if not unq or b != unq[-1]:
+                unq.append(b)
+                cnt.append(1)
+            else:
+                cnt[-1] += 1
+
+        n = len(unq)
+        dp = [[0] * i for i in range(1, n + 1)] # [i][j] from j to i max
+
+        pre = defaultdict(list)
+        for i, b in enumerate(unq):
+            pre[b].append(i)
+            dp[i][i] = cnt[i] ** 2
+            for j in range(i - 1, -1, -1):
+                theMax = dp[i - 1][j] + cnt[i] ** 2
+
+                npre = len(pre[b]) if unq[j] != unq[i] else len(pre[b]) - 1
+                for kk in range(npre - 1, -1, -1):
+                    k = pre[b][kk]
+                    if k > j:
+                        theMax = max(theMax, dp[i][k] + dp[k - 1][j])
+                    else:
+                        break
+
+                if unq[j] == unq[i]:
+                    poss = pre[b][kk:]
+                    nposs = len(poss)
+                    span = []
+                    for p in range(nposs - 1):
+                        span.append(dp[poss[p + 1] - 1][poss[p] + 1])
+                    total = sum(span)
+                    count_k = [cnt[p] for p in poss]
+                    total_k = sum(count_k)
+                    theMax = max(theMax, total + total_k ** 2)
+
+                    left_k = 0
+                    for ki in range(nposs - 2):
+                        left_k += count_k[ki]
+                        right_k = total_k - left_k
+                        left_right = total - span[ki]
+                        for kj in range(ki + 2, nposs):
+                            left_right -= span[kj - 1]
+                            right_k -= count_k[kj - 1]
+                            theMax = max(theMax, dp[poss[kj] - 1][poss[ki] + 1] \
+                                         + left_right + (left_k + right_k) ** 2)
+
+                dp[i][j] = theMax
+
+        return dp[-1][0]
+
         # # TLE 20/60
         # @memo
         # def dfs(*boxes):
@@ -118,5 +168,5 @@ class Solution(object):
 
 assert Solution().removeBoxes([1, 3, 2, 2, 2, 3, 4, 3, 1]) == 23
 assert Solution().removeBoxes([1, 3, 2, 2, 2, 3, 4, 2, 3, 1]) == 26
-print(Solution().removeBoxes([8, 1, 2, 10, 8, 5, 1, 10, 8, 4]))
+assert Solution().removeBoxes([8, 1, 2, 10, 8, 5, 1, 10, 8, 4]) == 16
 print(Solution().removeBoxes([3, 8, 8, 5, 5, 3, 9, 2, 4, 4, 6, 5, 8, 4, 8, 6, 9, 6, 2, 8, 6, 4, 1, 9, 5, 3, 10, 5, 3, 3, 9, 8, 8, 6, 5, 3, 7, 4, 9, 6, 3, 9, 4, 3, 5, 10, 7, 6, 10, 7]))
