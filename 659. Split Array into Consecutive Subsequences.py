26b1a6c0eaefdcaa902cdf6712c3d8796a0918e9
wfj <wufangjie1223@126.com>
2017-08-14 07:58:17 +0800

    235ms -> 92ms, old method is slow when nums[-1] is much larger than nums[0]

--- 659. Split Array into Consecutive Subsequences.py
+++ 659. Split Array into Consecutive Subsequences.py
@@ -1,26 +1,50 @@
-from collections import Counter
-
-
 class Solution(object):
     def isPossible(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        lo, hi = nums[0], nums[-1] + 1
-        count = Counter(nums)
-        c1, c2, c3 = count[lo], 0, 0 # length 1, 2, other
+        c1 = c2 = c3 = 0 # length 1, 2, other
+        n = len(nums)
+        i, j = 0, 1
+        nums.append(nums[-1] - 1)
 
-        for i in range(lo + 1, hi):
-            c = count.get(i, 0)
-            if c1 + c2 > c:
-                return False
+        while i < n:
+            while nums[j] == nums[i]:
+                j += 1
 
-            left = max(0, c - c1 - c2 - c3)
-            c1, c2, c3 = left, c1, c - left - c1
+            c = j - i
+            if nums[i] - nums[i - 1] > 1:
+                if c1 > 0 or c2 > 0:
+                    return False
+                c1, c2, c3 = c, 0, 0
+            else:
+                if c1 + c2 > c:
+                    return False
+                left = max(0, c - c1 - c2 - c3)
+                c1, c2, c3 = left, c1, c - left - c1
+            i = j
+            j += 1
 
         return c1 == c2 == 0
 
+
+        # # NOTE: following method is slow when nums = [1, 2, 3, 9999997, 9999998, 9999999]
+        # from collections import Counter
+
+        # lo, hi = nums[0], nums[-1] + 1
+        # count = Counter(nums)
+        # c1, c2, c3 = count[lo], 0, 0 # length 1, 2, other
+
+        # for i in range(lo + 1, hi):
+        #     c = count.get(i, 0)
+        #     if c1 + c2 > c:
+        #         return False
+
+        #     left = max(0, c - c1 - c2 - c3)
+        #     c1, c2, c3 = left, c1, c - left - c1
+
+        # return c1 == c2 == 0
 
 
         # lo, hi = nums[0], nums[-1] + 1
@@ -43,5 +67,6 @@ class Solution(object):
 print(Solution().isPossible([1,2,3,3,4,5]), True)
 print(Solution().isPossible([1,2,3,3,4,4,5,5]), True)
 print(Solution().isPossible([1,2,3,4,4,5]), False)
-
-print(Solution().isPossible([1,2]))
+print(Solution().isPossible([1,2]), False)
+print(Solution().isPossible([1, 2, 3, 9999997, 9999998, 9999999]), True)
+print(Solution().isPossible([3,4,4,5,6,7,8,9,10,11]), False)
