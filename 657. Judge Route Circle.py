5ea7e0225104a81d6b72e3840420bbfe7664feef
wfj <wufangjie1223@126.com>
2017-08-14 06:14:27 +0800

    shorten and speed up

--- 657. Judge Route Circle.py
+++ 657. Judge Route Circle.py
@@ -1,20 +1,27 @@
+from collections import Counter
+
+
 class Solution(object):
     def judgeCircle(self, moves):
         """
         :type moves: str
         :rtype: bool
         """
-        pos = [0, 0]
-        for step in moves:
-            if step == 'U':
-                pos[1] += 1
-            elif step == 'D':
-                pos[1] -= 1
-            elif step == 'L':
-                pos[0] -= 1
-            else:
-                pos[0] += 1
-        return pos[0] == 0 and pos[1] == 0
+        count = Counter(moves)
+        return (count.get('U', 0) == count.get('D', 0)
+                and count.get('L', 0) == count.get('R', 0))
+
+        # pos = [0, 0]
+        # for step in moves:
+        #     if step == 'U':
+        #         pos[1] += 1
+        #     elif step == 'D':
+        #         pos[1] -= 1
+        #     elif step == 'L':
+        #         pos[0] -= 1
+        #     else:
+        #         pos[0] += 1
+        # return pos[0] == 0 and pos[1] == 0
 
 print(Solution().judgeCircle('UD'))
 print(Solution().judgeCircle('LL'))
