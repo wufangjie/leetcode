981f8995bf597ec740243188881ec3a361a97369
wfj <wufangjie1223@126.com>
2017-08-20 16:31:38 +0800

    speed up, change list to set

--- 663. Equal Tree Partition.py
+++ 663. Equal Tree Partition.py
@@ -1,10 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution(object):
     def checkEqualTree(self, root):
         """
@@ -14,25 +7,26 @@ class Solution(object):
         if not root:
             return False
 
-        subSum = []
+        subSum = set()
 
         def dfs(node):
             if node is None:
                 return 0
-            else:
-                subSum.append(node.val + dfs(node.left) + dfs(node.right))
-                return subSum[-1]
+            ret = node.val + dfs(node.left) + dfs(node.right)
+            if node is not root:
+                subSum.add(ret)
+            return ret
 
-        dfs(root)
-        if subSum[-1] & 1:
+        total = dfs(root)
+        if total & 1:
             return False
-        elif subSum[-1] == 0:
-            return 0 in subSum[:-1]
         else:
-            return subSum[-1] // 2 in subSum
+            return total >> 1 in subSum
 
+# maybe float, but those static language's defination is int
+# and the change is easy
 
-
+from utils import TreeNode
 root = TreeNode(5, TreeNode(10, TreeNode(10, TreeNode(2), TreeNode(3))))
 
 print(Solution().checkEqualTree(root))
