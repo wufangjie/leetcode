694076399c4baf45933606d86f8c92ed15007821
wfj <wufangjie1223@126.com>
2017-08-27 21:47:24 +0800

    clean and speed up

--- 047. Permutations II.py
+++ 047. Permutations II.py
@@ -1,25 +1,10 @@
-'''
-Given a collection of numbers that might contain duplicates, return all possible unique permutations.
-
-For example,
-[1,1,2] have the following unique permutations:
-
-[
-  [1,1,2],
-  [1,2,1],
-  [2,1,1]
-]
-'''
-
-
 def permute_rec(nums):
     if nums:
         pre = None
-        nums_set = set(nums)
         for i, elem in enumerate(nums):
             if elem != pre:
                 pre = elem
-                for p in permute_rec(nums[:i] + nums[i + 1:]):
+                for p in permute_rec(nums[:i] + nums[i+1:]):
                     p.append(elem)
                     yield p
                     # yield [elem] + p  # about twice times
