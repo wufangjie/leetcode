d328de6cea5acc2f49e972ade4ce7d4ac0deb6a3
wfj <wufangjie1223@126.com>
2017-09-26 06:52:57 +0800

    prune and early stop

--- 681. Next Closest Time.py
+++ 681. Next Closest Time.py
@@ -4,33 +4,37 @@ class Solution(object):
         :type time: str
         :rtype: str
         """
-        limit = [int(time[0]) * 600, int(time[:2]) * 60]
-        limit.append(limit[-1] + int(time[3]) * 10)
-        limit.append(limit[-1] + int(time[4]))
-
-        t0 = 60 * int(time[:2]) + int(time[3:])
-        digits = set(map(int, time[:2] + time[3:]))
-        theMin = float('inf')
-        ret = '{0}{0}:{0}{0}'.format(min(digits))
+        # python2's True False is slow
+        limit = tuple(map(int, time[:2] + time[3:]))
+        digits = sorted(set(limit))
 
         for d1 in digits:
-            num = d1 * 600
-            if d1 > 2 or num < limit[0]:
+            if d1 > 2:
+                break
+            if d1 < limit[0]:
                 continue
+            equal = 1 if d1 == limit[0] else 0
             for d2 in digits:
-                num2 = num + d2 * 60
-                if (d1 == 2 and d2 > 3) or num2 < limit[1]:
-                    continue
-                for d3 in digits:
-                    num3 = num2 + d3 * 10
-                    if d3 > 5 or num3 < limit[2]:
+                if (d1 == 2 and d2 > 3):
+                    break
+                if equal:
+                    if d2 < limit[1]:
                         continue
+                    elif d2 > limit[1]:
+                        equal = 0
+                for d3 in digits:
+                    if d3 > 5:
+                        break
+                    if equal:
+                        if d3 < limit[2]:
+                            continue
+                        elif d3 > limit[2]:
+                            equal = 0
                     for d4 in digits:
-                        num4 = num3 + d4
-                        if num4 > t0 and num4 < theMin:
-                            theMin = num4
-                            ret = '{}{}:{}{}'.format(d1, d2, d3, d4)
-        return ret
+                        if not equal or d4 > limit[3]:
+                            return '{}{}:{}{}'.format(d1, d2, d3, d4)
+        return '{0}{0}:{0}{0}'.format(digits[0])
+
 
 
 
