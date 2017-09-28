fe91f64b90b56155bc71852dd88277199f6b65b5
wfj <wufangjie1223@126.com>
2017-09-28 10:06:40 +0800

    for the description's Update (2017-09-26)

--- 684. Redundant Connection.py
+++ 684. Redundant Connection.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
-        self.parent = list(range(2001))
+        self.parent = list(range(len(edges) + 1))
         for u, v in edges:
             pu, pv = self.find(u), self.find(v)
             if pu == pv:
@@ -17,53 +17,14 @@ class Solution(object):
         return self.parent[u]
 
 
-    # NOTE: if the description is right (a rooted tree),
-    # then it is not easy to solve the two root thing
-    def findRedundantConnection2(self, edges):
-        if len(edges) == 2:
-            return edges[1]
-        parent = {}
-        for i, (u, v) in enumerate(edges):
-            if v in parent:
-                p0 = parent[v]
-                if p0 in parent:
-                    return [u, v]
-                for u1, v1 in edges[i+1:]:
-                    if v1 == p0:
-                        return [u, v]
-                if sum(1 for u1, v1 in edges if u1 == p0) == 1:
-                    return [p0, v]
-                else:
-                    return [u, v]
-            else:
-                p = u
-                while p in parent:
-                    p = parent[p]
-                    if p == v:
-                        return [u, v]
-                parent[v] = u
+
+print(Solution().findRedundantConnection([[1,2], [1,3], [2,3]]), [2, 3])
+print(Solution().findRedundantConnection([[2,3],[5,2],[1,5],[4,2],[4,1]]))
 
 
+# NOTE: the description is changed and much clearer:
+# not a rooted tree
+# no duplicate edges, otherwise not connected (N vertices and N - 1 edges)
+# no cycles (no self-loop)
 
-
-# print(Solution().findRedundantConnection([[1,2], [1,3], [2,3]]), [2, 3])
-# print(Solution().findRedundantConnection([[2,3],[5,2],[1,5],[4,2],[4,1]]))
-
-
-# assume not dumplicate
-# NOTE:
-# It seems like the description is wrong. # I think the answer is wrong
-# need not to be a rooted tree, a child may have two parents, as follows:
-
-# Input:[[2,3],[5,2],[1,5],[4,2],[4,1]]
-# Output:[4,2]
-# Expected:[4,1]
-
-
-# NOTE2:
-# assume the description is correct, then we must consider the two root thing:
-[[1, 2], [3, 2], [4, 3]] # for example
-
-print(Solution().findRedundantConnection2([[1,2], [1,3], [3,1]]), [3, 1])
-print(Solution().findRedundantConnection2([[1, 2], [3, 2], [4, 3]]), [1, 2])
-print(Solution().findRedundantConnection2([[1, 2], [2, 3], [4, 3], [4, 5], [1, 4]]), [4, 3])
+# my original rooted tree solution is wrong, see 685. Redundant Connection II.py
